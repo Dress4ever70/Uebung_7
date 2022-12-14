940ab304574660727ac7c1ec518b57507150e72f
49176 <49176@LAPTOP-L5T72IVA>
2022-12-14 10:21:24 +0100

    Projektdateien hinzufügen.

---
+++ Uebung_7.cpp
@@ -0,0 +1,11 @@
+// Uebung_7.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello World!");
+
+}
+
